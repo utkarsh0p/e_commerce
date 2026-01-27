a697c2ee40c024f0c8dd83cdb1ae9ec021dcab5c
Utkarsh Singh <utkarshdevendrasingh@gmail.com>
2026-01-27 20:40:22 +0530

    initialised project  and added server.js

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+//imports
+import cors from 'cors'
+import express, { json } from "express"
+import userModel from "./models/user.model.js"
+
+//file imports 
+import authRoute from "./routes/auth.route.js"
+import connectDB from "./lib/db.js"
+
+//defining servers
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+//dotenv variables
+const port = process.env.PORT || 5000
+
+
+//these are the routes
+server.use("/api/auth", authRoute)
+
+console.log(userModel)
+await connectDB()
+server.listen(port, () => {
+  console.log("Server running on http://localhost:5000/")
+})
